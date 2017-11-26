35ec65fad113770139d3f6666585b9760f00faac
Ikey Doherty <ikey@solus-project.com>
2017-11-26 11:11:02 +0000

    Bump v17
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- mkrelease.sh
+++ mkrelease.sh
@@ -5,7 +5,7 @@ git submodule init
 git submodule update
 
 # Script for ikey because he went with meson. *shrug*
-VERSION="16"
+VERSION="17"
 NAME="mate-desktop-branding"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"

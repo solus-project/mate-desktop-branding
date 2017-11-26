afe5934d7ddb0134dcca11b75a6dfdfacdec2aa8
Ikey Doherty <ikey@solus-project.com>
2017-11-26 10:44:34 +0000

    Bump to version 16
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- mkrelease.sh
+++ mkrelease.sh
@@ -5,7 +5,7 @@ git submodule init
 git submodule update
 
 # Script for ikey because he went with meson. *shrug*
-VERSION="15"
+VERSION="16"
 NAME="mate-desktop-branding"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
